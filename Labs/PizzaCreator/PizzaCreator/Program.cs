95f825db61e59d7a984e13879c01fa4971842294
mitchelldavis553 <mitchell.davis553@my.tccd.edu>
2018-08-27 20:29:35 -0500

    Starting Lab 1 Pizza Creator

---
+++ Labs/PizzaCreator/PizzaCreator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMenu();
+        }
+
+        private static void DisplayMenu()
+        {
+            Console.WriteLine("N)ew Order");
+            Console.WriteLine("M)odify Order");
+            Console.WriteLine("D)isplay Order");
+            Console.WriteLine("Q)uit");
+        }
+    }
+}
