7d9c5070f3b7602a033a927245e8f574088d1408
mitchell.davis553 <mitchell.davis553@NEITD-123419>
2018-08-29 18:52:29 -0500

    More additions

--- Classwork/Section1/Section1/Program.cs
+++ Classwork/Section1/Section1/Program.cs
@@ -10,8 +10,11 @@ namespace Section1
     {
         static void Main( string[] args )
         {
-            //DisplayMenu();
-            PlayWithStrings();
+            do
+                DisplayMenu();
+            while (true);
+
+            //PlayWithStrings();
         }
 
         private static void PlayWithStrings()
@@ -40,26 +43,83 @@ namespace Section1
             string firstName = "Bob";
             string lastName = "Smith";
             string name = firstName + " " + lastName;
+
+            //Strings are immutable - this produces a new string
+            //name = "Hello " + name;
+
+            // String Formatting
+            Console.WriteLine("Hello " + name); // Approach 1
+            Console.WriteLine("Hello {0} {1}", firstName, lastName); // Approach 2
+            string str = String.Format("Hello {0} {1}", firstName, lastName); // Approach 3
+            Console.WriteLine();
+
+            // Approach 4  Interpreted Strings
+            Console.WriteLine($"Hello {firstName} {lastName}");
+
+            // Null vs Empty
+            string missing = null; // I HAVE NO VALUE
+            string empty = ""; // these are not the same value // Empty String 
+            string empty2 = String.Empty;
+
+
+            //Checking for Empty Strings
+            //if (firstName.Length == 0)
+            // if (firstName != null && firstName != "")
+            if (!String.IsNullOrEmpty(firstName))
+                Console.WriteLine(firstName);
+
+            //Other Stuff
+            string upperName = firstName.ToUpper();
+            string lowerName = firstName.ToLower();
+
+            //String Comparison
+            bool areEqual = firstName == lastName;
+            areEqual = firstName.ToLower() == lastName.ToLower(); // Inefficient also STRINGS ARE IMMUTABLE
+            areEqual = String.Compare(firstName, lastName, true) == 0; // Returns 3 things:  INT:   <0  a < b     0 a == b    >0  a > b     "true means to ignore casing"
+
+            bool startsWithA = firstName.StartsWith("A");
+            bool endsWithA = firstName.EndsWith("A");
+            bool hasA = firstName.IndexOf("A") >= 0;
+            string subset = firstName.Substring(4);
+
+            //Clean up
+            string cleanMe = firstName.Trim(); //removes whitespace  //TrimStart, TrimEnd
+            string makeLonger = firstName.PadLeft(20); //PadRight
+
         }
 
-        private static void DisplayMenu()
+        private static bool DisplayMenu()
         {
-            Console.WriteLine("A)dd Movie");
-            Console.WriteLine("E)dit Movie");
-            Console.WriteLine("D)elete Movie");
-            Console.WriteLine("V)iew Movies");
-            Console.WriteLine("Q)uit");
-
-            string input = Console.ReadLine();
-            switch (input[0])
+            while (true)
             {
-                case 'A': AddMovie(); break;
-                case 'E': EditMovie(); break;
-                case 'D': DeleteMovie(); break;
-                case 'V': ViewMovie(); break;
-                case 'Q': ; break;
+                Console.WriteLine("A)dd Movie");
+                Console.WriteLine("E)dit Movie");
+                Console.WriteLine("D)elete Movie");
+                Console.WriteLine("V)iew Movies");
+                Console.WriteLine("Q)uit");
 
-                default: Console.WriteLine("Please enter a valid value."); break;
+                string input = Console.ReadLine();
+                switch (input[0])
+                {
+                    case 'a': 
+                    case 'A': AddMovie(); return true;
+
+                    case 'e': 
+                    case 'E': EditMovie(); return true;
+
+                    case 'd': 
+                    case 'D': DeleteMovie(); return true;
+
+                    case 'v': 
+                    case 'V': ViewMovie(); return true;
+
+                    case 'q': 
+                    case 'Q': return false;
+
+                    default:
+                    Console.WriteLine("Please enter a valid value.");
+                    break;
+                };
             };
         }
 
