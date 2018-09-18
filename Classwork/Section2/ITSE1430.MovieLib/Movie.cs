711c9dbf2f858b3cbe884344d64448f4f8fa63cb
mitchell.davis553 <mitchell.davis553@NEITD-123419>
2018-09-17 19:04:53 -0500

    Starting Section 2

---
+++ Classwork/Section2/ITSE1430.MovieLib/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSE1430.MovieLib
+{
+    class Movie
+    {
+        public string Name;
+        //public System.String Name;
+
+    }
+}
