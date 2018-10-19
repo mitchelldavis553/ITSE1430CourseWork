6677b2dd1fc43b3ea603f282367c2317615ae8fb
mitchelldavis553 <mitchell.davis553@my.tccd.edu>
2018-10-19 15:22:21 -0500

    Layout Prettying

--- Labs/CharacterCreator.Winforms/CharacterCreator.Winforms/CharacterForm.Designer.cs
+++ Labs/CharacterCreator.Winforms/CharacterCreator.Winforms/CharacterForm.Designer.cs
@@ -57,7 +57,8 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 46);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(33, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
@@ -65,16 +66,21 @@
             // 
             // _txtName
             // 
-            this._txtName.Location = new System.Drawing.Point(75, 39);
+            this._txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._txtName.BackColor = System.Drawing.SystemColors.Window;
+            this._txtName.ForeColor = System.Drawing.Color.Black;
+            this._txtName.Location = new System.Drawing.Point(75, 12);
             this._txtName.Name = "_txtName";
-            this._txtName.Size = new System.Drawing.Size(118, 20);
+            this._txtName.Size = new System.Drawing.Size(126, 20);
             this._txtName.TabIndex = 0;
             this._txtName.Validating += new System.ComponentModel.CancelEventHandler(this.OnValidateName);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 84);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(12, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 2;
@@ -82,7 +88,11 @@
             // 
             // _professionComboBox
             // 
+            this._professionComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._professionComboBox.BackColor = System.Drawing.SystemColors.Window;
             this._professionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this._professionComboBox.ForeColor = System.Drawing.Color.Black;
             this._professionComboBox.FormattingEnabled = true;
             this._professionComboBox.Items.AddRange(new object[] {
             "Fighter",
@@ -90,16 +100,17 @@
             "Priest",
             "Rogue",
             "Wizard"});
-            this._professionComboBox.Location = new System.Drawing.Point(75, 81);
+            this._professionComboBox.Location = new System.Drawing.Point(73, 51);
             this._professionComboBox.Name = "_professionComboBox";
-            this._professionComboBox.Size = new System.Drawing.Size(120, 21);
+            this._professionComboBox.Size = new System.Drawing.Size(128, 21);
             this._professionComboBox.TabIndex = 1;
             this._professionComboBox.Validating += new System.ComponentModel.CancelEventHandler(this.OnValidateProfession);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(20, 127);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(35, 96);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(33, 13);
             this.label3.TabIndex = 4;
@@ -107,6 +118,10 @@
             // 
             // _raceComboBox
             // 
+            this._raceComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._raceComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this._raceComboBox.ForeColor = System.Drawing.Color.Black;
             this._raceComboBox.FormattingEnabled = true;
             this._raceComboBox.Items.AddRange(new object[] {
             "Dwarf",
@@ -114,16 +129,18 @@
             "Gnome",
             "Half Elf",
             "Human"});
-            this._raceComboBox.Location = new System.Drawing.Point(77, 127);
+            this._raceComboBox.Location = new System.Drawing.Point(73, 93);
             this._raceComboBox.Name = "_raceComboBox";
-            this._raceComboBox.Size = new System.Drawing.Size(117, 21);
+            this._raceComboBox.Size = new System.Drawing.Size(125, 21);
             this._raceComboBox.TabIndex = 2;
             this._raceComboBox.Validating += new System.ComponentModel.CancelEventHandler(this.OnValidateRace);
             // 
             // label4
             // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(151, 195);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(8, 179);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(54, 13);
             this.label4.TabIndex = 6;
@@ -131,7 +148,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(41, 358);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(18, 283);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(107, 29);
             this.button1.TabIndex = 9;
@@ -141,7 +160,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(154, 358);
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(131, 283);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(116, 28);
             this.button2.TabIndex = 10;
@@ -151,17 +172,21 @@
             // 
             // _txtStrength
             // 
-            this._txtStrength.Location = new System.Drawing.Point(48, 258);
+            this._txtStrength.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this._txtStrength.ForeColor = System.Drawing.Color.Black;
+            this._txtStrength.Location = new System.Drawing.Point(18, 230);
             this._txtStrength.Name = "_txtStrength";
-            this._txtStrength.Size = new System.Drawing.Size(100, 20);
+            this._txtStrength.Size = new System.Drawing.Size(45, 20);
             this._txtStrength.TabIndex = 4;
             this._txtStrength.Text = "50";
             this._txtStrength.Validating += new System.ComponentModel.CancelEventHandler(this.OnValidateAttributes);
             // 
             // label5
             // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(75, 239);
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(15, 214);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(47, 13);
             this.label5.TabIndex = 10;
@@ -169,8 +194,10 @@
             // 
             // label6
             // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(235, 238);
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(72, 214);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(61, 13);
             this.label6.TabIndex = 11;
@@ -178,8 +205,10 @@
             // 
             // label7
             // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(385, 229);
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(144, 214);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(34, 13);
             this.label7.TabIndex = 12;
@@ -187,8 +216,10 @@
             // 
             // label8
             // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(547, 238);
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(203, 214);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(62, 13);
             this.label8.TabIndex = 13;
@@ -196,8 +227,10 @@
             // 
             // label9
             // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(725, 229);
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(271, 214);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(50, 13);
             this.label9.TabIndex = 14;
@@ -205,36 +238,44 @@
             // 
             // _txtIntelligence
             // 
-            this._txtIntelligence.Location = new System.Drawing.Point(204, 257);
+            this._txtIntelligence.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this._txtIntelligence.ForeColor = System.Drawing.Color.Black;
+            this._txtIntelligence.Location = new System.Drawing.Point(75, 230);
             this._txtIntelligence.Name = "_txtIntelligence";
-            this._txtIntelligence.Size = new System.Drawing.Size(100, 20);
+            this._txtIntelligence.Size = new System.Drawing.Size(45, 20);
             this._txtIntelligence.TabIndex = 5;
             this._txtIntelligence.Text = "50";
             this._txtIntelligence.Validating += new System.ComponentModel.CancelEventHandler(this.OnValidateAttributes);
             // 
             // _txtAgility
             // 
-            this._txtAgility.Location = new System.Drawing.Point(350, 257);
+            this._txtAgility.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this._txtAgility.ForeColor = System.Drawing.Color.Black;
+            this._txtAgility.Location = new System.Drawing.Point(147, 230);
             this._txtAgility.Name = "_txtAgility";
-            this._txtAgility.Size = new System.Drawing.Size(100, 20);
+            this._txtAgility.Size = new System.Drawing.Size(45, 20);
             this._txtAgility.TabIndex = 6;
             this._txtAgility.Text = "50";
             this._txtAgility.Validating += new System.ComponentModel.CancelEventHandler(this.OnValidateAttributes);
             // 
             // _txtConstitution
             // 
-            this._txtConstitution.Location = new System.Drawing.Point(523, 257);
+            this._txtConstitution.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this._txtConstitution.ForeColor = System.Drawing.Color.Black;
+            this._txtConstitution.Location = new System.Drawing.Point(206, 230);
             this._txtConstitution.Name = "_txtConstitution";
-            this._txtConstitution.Size = new System.Drawing.Size(100, 20);
+            this._txtConstitution.Size = new System.Drawing.Size(45, 20);
             this._txtConstitution.TabIndex = 7;
             this._txtConstitution.Text = "50";
             this._txtConstitution.Validating += new System.ComponentModel.CancelEventHandler(this.OnValidateAttributes);
             // 
             // _txtCharisma
             // 
-            this._txtCharisma.Location = new System.Drawing.Point(685, 257);
+            this._txtCharisma.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this._txtCharisma.ForeColor = System.Drawing.Color.Black;
+            this._txtCharisma.Location = new System.Drawing.Point(274, 230);
             this._txtCharisma.Name = "_txtCharisma";
-            this._txtCharisma.Size = new System.Drawing.Size(100, 20);
+            this._txtCharisma.Size = new System.Drawing.Size(45, 20);
             this._txtCharisma.TabIndex = 8;
             this._txtCharisma.Text = "50";
             this._txtCharisma.Validating += new System.ComponentModel.CancelEventHandler(this.OnValidateAttributes);
@@ -242,7 +283,8 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(388, 83);
+            this.label10.ForeColor = System.Drawing.Color.Black;
+            this.label10.Location = new System.Drawing.Point(8, 133);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(60, 13);
             this.label10.TabIndex = 15;
@@ -250,10 +292,14 @@
             // 
             // _txtDescription
             // 
-            this._txtDescription.Location = new System.Drawing.Point(455, 81);
+            this._txtDescription.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._txtDescription.ForeColor = System.Drawing.Color.Black;
+            this._txtDescription.Location = new System.Drawing.Point(73, 130);
             this._txtDescription.Multiline = true;
             this._txtDescription.Name = "_txtDescription";
-            this._txtDescription.Size = new System.Drawing.Size(320, 20);
+            this._txtDescription.Size = new System.Drawing.Size(328, 26);
             this._txtDescription.TabIndex = 3;
             // 
             // _errors
@@ -266,7 +312,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(943, 450);
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(407, 324);
             this.Controls.Add(this._txtDescription);
             this.Controls.Add(this.label10);
             this.Controls.Add(this._txtCharisma);
@@ -290,6 +337,7 @@
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(423, 363);
             this.Name = "CharacterForm";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
