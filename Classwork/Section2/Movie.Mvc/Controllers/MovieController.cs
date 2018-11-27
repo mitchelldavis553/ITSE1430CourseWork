887e0f53b581f260b0bf5a2634ddb8513da023d4
mitchell.davis553 <mitchell.davis553@NEITD-123419>
2018-11-26 19:12:08 -0600

    Starting Web App stuff

---
+++ Classwork/Section2/Movie.Mvc/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movie.Mvc.Controllers
+{
+    public class MovieController : Controller
+    {
+        //Verbs:
+        //Post/Put modifies data, create and update
+        //Delete deletes data
+        [HttpGet] // Retrieval of data, NEVER MODIFIES DATA, SHOULD ONLY RETRIEVE
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
