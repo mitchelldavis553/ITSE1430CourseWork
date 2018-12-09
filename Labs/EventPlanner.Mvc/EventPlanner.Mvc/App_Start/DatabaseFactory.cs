f749f7ac4297b507895f296c759d2cc64d24808f
mitchelldavis553 <mitchell.davis553@my.tccd.edu>
2018-12-08 19:13:19 -0600

    adding attributes

---
+++ Labs/EventPlanner.Mvc/EventPlanner.Mvc/App_Start/DatabaseFactory.cs
@@ -0,0 +1,45 @@
+﻿using EventPlanner.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventPlanner.Mvc.App_Start
+{
+    public class DatabaseFactory
+    {
+        static DatabaseFactory()
+        {
+            var db = new MemoryEventDatabase();
+
+            var events = new[]
+            {
+                new ScheduledEvent()
+                {
+                    Name = "Christmas Time",
+                    Description = "Cold Weather, and conventional family outings.",
+                    StartDate = DateTime.Parse("12/01/2018"),
+                    EndDate = DateTime.Parse("12/31/2018"),
+                    IsPublic = true,
+                },
+                new ScheduledEvent()
+                {
+                    Name = "Birthday",
+                    Description = "My Birthday",
+                    StartDate = DateTime.Parse("04/07/2019"),
+                    EndDate = DateTime.Parse("04/07/2019"),
+                    IsPublic = false,
+                },
+            };
+
+            foreach (var value in events)
+                db.Add(value);
+
+            Database = db;
+        }
+        private DatabaseFactory()
+        { }
+
+        public static IEventDatabase Database { get; }
+    }
+}
---
+++ Labs/EventPlanner.Mvc/EventPlanner.Mvc/Models/EventModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EventPlanner.Mvc.Models
+{
+    public class EventModel
+    {
+        public EventModel()
+        { }
+
+        [Range(0, Int32.MaxValue)]
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name is required")]
+        public string Name
+        {
+            get => _name ?? "";
+            set => _name = value;
+        }
+        private string _name;
+
+        public string Description
+        {
+            get => _description ?? "";
+            set => _description = value;
+        }
+        private string _description;
+
+        [Display(Name = "Start Date")]
+        []
+        public DateTime StartDate { get; set; }
+      
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+
+        public bool IsPublic { get; set; }
+    }
+}
